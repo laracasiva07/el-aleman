400f26c3cebd5bb6ef83f0c0fb7e0fb97da4ae4d
Lara Casiva <laracasiva@hotmail.com>
2026-09-23 21:28:21 -0300

    correccion render

--- backend/src/app.js
+++ backend/src/app.js
@@ -24,7 +24,8 @@ const allowedOrigins = [
   process.env.FRONTEND_URL,
   'http://localhost:5173',
   'http://localhost:3000',
-  'http://127.0.0.1:5173'
+  'http://127.0.0.1:5173',
+  'https://el-aleman-ten.vercel.app'
 ].filter(Boolean);
 
 const corsOptions = {
